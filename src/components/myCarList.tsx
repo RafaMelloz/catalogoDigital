31178e34c29568aad4f9b471f73aee631d83e84f
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2024-05-02 19:06:22 -0300

    carrinho vazio

--- src/components/myCarList.tsx
+++ src/components/myCarList.tsx
@@ -27,15 +27,22 @@ export function MyCarList() {
         <main className="flex justify-between max-w-5xl m-auto pt-8  max-md:flex-direction: column max-md:flex-col max-md:items-center max-md:gap-3  max-md:px-3">
             <div className="bg-gray-800/30 max-w-xl w-full p-4">
                 <h3 className="text-lg font-bold tracking-wide">Meu carrinho</h3>
+
                 <ul>
-                    {carShop.map((product) =>
-                        <ShopCarCards
-                            key={product.id}
-                            product={product}
-                            removeFromCar={removeFromCar}
-                            setCarShop={setCarShop}
-                        />
-                    )}
+                    { carShop.length != 0 ? 
+                        (
+                            carShop.map((product) =>
+                                <ShopCarCards
+                                    key={product.id}
+                                    product={product}
+                                    removeFromCar={removeFromCar}
+                                    setCarShop={setCarShop}
+                                />
+                            )
+                        )
+                    :
+                     <li className="text-center mt-8 text-xl font-bold">Carrinho Vazio</li>
+                    }
                 </ul>
             </div>
 
